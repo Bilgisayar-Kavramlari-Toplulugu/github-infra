9eb53ea735c2b716358fe124300e27e1554e749c
Feyz SARI <flovearth@protonmail.com>
2025-11-19 12:34:49 +0000

    adds use_lockfile

--- versions.tf
+++ versions.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket  = "Bilgisayar-Kavramlari-Toplulugu"
+    bucket  = "bilgisayar-kavramlari-toplulugu"
     key     = "terraform.tfstate"
     region  = "eu-west-2"
     encrypt = true
+    use_lockfile = true
   }
 
   required_version = ">= 1.0"
